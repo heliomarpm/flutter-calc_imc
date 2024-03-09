c23376be16fc0f65b2cf85f62861e2b5feb20e59
Heliomar P. Marques <heliomarpm@gmail.com>
2024-03-09 13:07:00 -0300

    add preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(DevicePreview(builder: (context)=> const MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
